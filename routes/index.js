152f024cde27217c8f88fdbcdd35a15c655f907d
mGrimley <mich@elgrimley.com>
2022-10-12 10:55:56 -0700

    Adds last bit of CSS for awhile (I promise)

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'All Recipes' });
+  res.render('recipes/index', { title: 'All Recipes' });
 });
 
 module.exports = router;
