9a61f643ecc7c6b02c4ed515bf58d6e4fe840210
mGrimley <mich@elgrimley.com>
2022-10-17 10:49:21 -0700

    Adds preliminary seed files

---
+++ data.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose')
+
+exports.recipes = [
+    {userName: 'Michael Grimley', userAvatar:'https://lh3.googleusercontent.com/a/ALm5wu0O1GYn66o9V-dpCOFWKSsfGfA-m8t5HPVzUtX3=s96-c', title: '', mainCategory: '', prepTime: 0, cookTime: 0, ingredients: ['', '', ''], steps: ['', '', '']},
+    {userName: 'Michael Grimley', userAvatar:'https://lh3.googleusercontent.com/a/ALm5wu0O1GYn66o9V-dpCOFWKSsfGfA-m8t5HPVzUtX3=s96-c', title: '', mainCategory: '', prepTime: 0, cookTime: 0, ingredients: ['', '', ''], steps: ['', '', '']},
+]
